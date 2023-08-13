Attempt to fix training unable to perform validation loss calculation after hitting a broken batch

valle/bin/trainer.py
@@ -694,23 +694,12 @@ def train_one_epoch(
             logging.warning(f"Error encountered: {str(e)}")
             display_and_save_batch(batch, params=params)
             # Clean up batch data from Memory and GPU
-            del batch["text_tokens"]
-            del batch["text_tokens_lens"]
-            del batch["audio_features"]
-            del batch["audio_features_lens"]
-            del batch
-            try:
-                del loss
-                del loss_info
-            except UnboundLocalError:
-                pass
             torch.cuda.empty_cache()
+        finally:
             # Continue training
-            pass
-
-        if world_size > 1:
-            # Ensure all GPUs continue after backpropagation at the same time
-            torch.distributed.barrier()
+            if world_size > 1:
+                # Ensure all GPUs continue after backpropagation at the same time
+                torch.distributed.barrier()
 
         if params.batch_idx_train >= params.accumulate_grad_steps:
             if (