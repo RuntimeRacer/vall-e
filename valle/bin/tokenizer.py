c2bcedfb9bb8c3e8281dc71b771500694350b1b4
RuntimeRacer <49956579+RuntimeRacer@users.noreply.github.com>
2024-03-16 12:52:48 +0100

    WIP: Full Parallelized Tokenization & Feature extraction

--- valle/bin/tokenizer.py
+++ valle/bin/tokenizer.py
@@ -310,7 +310,7 @@ if __name__ == "__main__":
     working_dir = Path(f"{args.output_dir}/work")
     if working_dir.exists():
         os.rmdir(working_dir)  # clear existing dir
-    os.makedirs(working_dir)
+    os.makedirs(working_dir, exist_ok=True)
 
     # Setup Symbol Table - reuse symbols file in case we want to extend existing training data with a new language
     if args.text_extractor:
